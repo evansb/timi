16e70d03779336b98fa8133e658f567bb4eec383
liuyang <melody9951213@gmail.com>
2015-09-09 23:36:45 +0800

    new schema

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,56 +1,50 @@
-var bookshelf = require('./bookshelf');
+import bookshelf from './bookshelf';
 
 bookshelf.knex.schema
-    .createTable('users', function (t) {
-        t.string('username').primary();
-        t.string('password').notNullable();
-        t.timestamps();
-    })
-    .createTable('vendors', function (t) {
-        t.increments().primary();
-        t.string('name').notNullable();
-        t.string('icon');
-        t.boolean('is_email').notNullable();
-        t.boolean('verifiable').notNullable();
-        t.timestamps();
-    })
-    .createTable('identities', function (t) {
-        t.increments().primary();
-        t.string('user_username').notNullable().references('users.username').onDelete('cascade').onUpdate('cascade');
-        t.integer('vendor_id').unsigned().notNullable().references('vendors.id').onDelete('cascade').onUpdate('cascade');
-        t.string('field').notNullable();
-        t.boolean('verified');
-        t.timestamps();
-    })
-    .createTable('cards', function (t) {
-        t.increments().primary();
-        t.string('user_username').notNullable().references('users.username').onDelete('cascade').onUpdate('cascade');
-        t.string('name').notNullable();
-        t.timestamps();
-    })
-    .createTable('cards_identities', function (t) {
-        t.integer('card_id').unsigned().notNullable().references('cards.id').onDelete('cascade').onUpdate('cascade');
-        t.integer('identity_id').unsigned().notNullable().references('identities.id').onDelete('cascade').onUpdate('cascade');
-        t.primary(['card_id', 'identity_id']);
-        t.timestamps();
-    })
-    .createTable('groups', function (t) {
-        t.increments().primary();
-        t.string('name').notNullable();
-        t.timestamps();
-    })
-    .createTable('roles', function (t) {
-        t.increments().primary();
-        t.string('name').notNullable();
-        t.timestamps();
-    })
-    .createTable('users_groups_roles', function (t) {
-        t.string('user_username').notNullable().references('users.username').onDelete('cascade').onUpdate('cascade');
-        t.integer('group_id').unsigned().notNullable().references('groups.id').onDelete('cascade').onUpdate('cascade');
-        t.integer('role_id').unsigned().notNullable().references('roles.id').onDelete('cascade').onUpdate('cascade');
-        t.primary(['user_username', 'group_id', 'role_id']);
-        t.timestamps();
-    }).
-    then(function () {
-        console.log('success');
-    });
+  .createTable('users', function (t) {
+    t.increments().primary();
+    t.string('email').unique().notNullable();
+    t.string('name');
+    t.timestamps();
+  })
+  .createTable('polls', function (t) {
+    t.increments().primary();
+    t.timestamp('deadline');
+    t.integer('owner_id').unsigned().notNullable().references('users.id').onDelete('cascade').onUpdate('cascade');
+    t.timestamps();
+  })
+  .createTable('polls_users', function (t) {
+    t.integer('poll_id').unsigned().notNullable().references('polls.id').onDelete('cascade').onUpdate('cascade');
+    t.integer('user_id').unsigned().notNullable().references('users.id').onDelete('cascade').onUpdate('cascade');
+    t.boolean('important').notNullable().defaultTo(false);
+    t.boolean('participated').notNullable().defaultTo(false);
+    t.boolean('confirmed').notNullable().defaultTo(false);
+    t.timestamps();
+
+    t.primary(['poll_id', 'user_id']);
+  })
+  .createTable('timeslots', function (t) {
+    t.increments().primary();
+    t.integer('poll_id').unsigned().notNullable().references('polls.id').onDelete('cascade').onUpdate('cascade');
+    t.timestamp('start').notNullable();
+    t.timestamp('end').notNullable();
+    t.timestamps();
+  })
+  .createTable('availabilities', function (t) {
+    t.integer('user_id').unsigned().notNullable().references('users.id').onDelete('cascade').onUpdate('cascade');
+    t.integer('timeslot_id').unsigned().notNullable().references('timeslots.id').onDelete('cascade').onUpdate('cascade');
+    t.integer('weight').unsigned().notNullable();
+    t.timestamps();
+
+    t.primary(['user_id', 'timeslot_id']);
+  })
+  .createTable('confirmations', function (t) {
+    t.integer('user_id').unsigned().notNullable().references('users.id').onDelete('cascade').onUpdate('cascade');
+    t.integer('timeslot_id').unsigned().notNullable().references('timeslots.id').onDelete('cascade').onUpdate('cascade');
+    t.timestamps();
+
+    t.primary(['user_id', 'timeslot_id']);
+  })
+  .then(function () {
+    console.log('success');
+  });
