e09c9c538a8b7d088d354ef2f674caef2679030f
Evan Sebastian <evanlhoini@gmail.com>
2015-09-09 21:38:34 +0800

    Change app name to timi

--- app/src/index.js
+++ app/src/index.js
@@ -5,7 +5,7 @@ import 'angular-sanitize'
 import 'angular-ui-router'
 import 'ionic';
 
-angular.module('assignment-3', ['ionic'])
+angular.module('timi', ['ionic'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
