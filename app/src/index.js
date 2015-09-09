9cf9f11cad58a94fb16448c51858d0b698016b82
Evan Sebastian <evanlhoini@gmail.com>
2015-09-10 00:13:34 +0800

    Define angular app structure

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,27 @@
+import app          from './app';
+import route        from './route';
+import startup      from './startup';
+import directives   from './directives';
+import controllers  from './controllers';
+import services     from './services';
 
-import app      from './app';
-import route    from './route';
-import startup  from './startup';
+// Setup routes
+app.config(route)
 
-app.config(route)   // Setup routes
-  .run(startup)    // Initialize the app
+// Load Services
+services.forEach((service) => {
+  app.service(service.name, service.fn);
+});
+
+// Load Controllers
+controllers.forEach((service) => {
+  app.controller(controller.name, controller.fn);
+});
+
+// Load Directives
+directives.forEach((service) => {
+  app.directive(directive.name, directive.fn);
+});
+
+// Initialize the app
+app.run(startup)
