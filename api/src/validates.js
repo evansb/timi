952a5798563872778bf24a56d816e495b6136fa6
liuyang <melody9951213@gmail.com>
2015-09-11 15:32:42 +0800

    add some new schema

--- api/src/validates.js
+++ api/src/validates.js
@@ -1,14 +1,16 @@
 import Joi from 'joi';
 import User from './models/user';
 
+var emailSchema = Joi.string().email();
+
 var userSchema = Joi.object().keys({
-  email: Joi.string().email().required(),
+  email: emailSchema.required(),
   password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).required(),
   name: Joi.string().alphanum().max(30)
 });
 
 var timeslotSchema = Joi.object().keys({
-  event_id: Joi.number().integer().min(1).required(),
+  event_id: Joi.number().integer().positive().required(),
   start: Joi.date().required(),
   end: Joi.date().required()
 }).min(1).unique();
@@ -16,15 +18,31 @@ var timeslotSchema = Joi.object().keys({
 var eventSchema = Joi.object().keys({
   name: Joi.string().max(50),
   deadline: Joi.date().min(new Date()),
-  owner_id: Joi.number().integer().min(1).required(),
-  participants: Joi.array().items(Joi.number().integer().min(1)).min(1).unique(),
+  owner_id: Joi.number().integer().positive().required(),
+  participants: Joi.array().items(Joi.number().integer().positive()).min(1).unique(),
   timeslots: Joi.array().items(timeslotSchema)
 });
 
+var availabilitySchema = Joi.object().keys({
+  user_id: Joi.number().integer().positive().required(),
+  timeslot_id: Joi.number().integer().positive().required(),
+  weight: Joi.number().integer().positive().required()
+});
+
+var availabilitiesSchema = Joi.array().items(availabilitySchema);
+
 exports.newUser = (user) => {
-  Joi.attempt(user, user_schema);
+  Joi.attempt(user, userSchema);
 };
 
 exports.newEvent = (event) => {
-  Joi.attempt(event, event_schema);
+  Joi.attempt(event, eventSchema);
 };
+
+exports.userEmail = (email) => {
+  Joi.attempt(email, emailSchema);
+}
+
+exports.userAvailabilities = (availabilities) => {
+  Joi.attempt(availabilities, emailSchema);
+}
