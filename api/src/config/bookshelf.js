ebee22709bb0c0895b481eec448b28eafc6e7cd2
liuyang <melody9951213@gmail.com>
2015-09-11 09:08:53 +0800

    what the hell

--- api/src/config/bookshelf.js
+++ api/src/config/bookshelf.js
@@ -4,8 +4,8 @@ var knex = require('knex')({
   client: 'postgresql',
   connection: {
     host: '127.0.0.1',
-    user: 'liuyang',
-    password: 'tainawujia',
+    user: 'your_db_username',
+    password: 'your_db_password',
     database: 'test',
     charset: 'utf8'
   }
