fb5953c6c405b53132857ac58a0b5f7983d92905
Evan Sebastian <evanlhoini@gmail.com>
2015-09-20 00:10:43 +0800

    Put owner name and deadline

--- app/src/common/timiEventCard.js
+++ app/src/common/timiEventCard.js
@@ -8,8 +8,14 @@ export default () => {
         <div class="item item-text-wrap">
           <h2>{{ event.name }}</h2>
           <div ng-if="event.isPending">
-            <p>{{ event.owner_id }}</p>
-            <p>Respond by {{ event.deadline }}</p>
+            <p>
+              <i class="icons ion-person"></i>
+              {{ event.owner.name }}
+            </p>
+            <p>
+              <i class="icons ion-ios-stopwatch"></i>
+              {{ event.deadline }}
+            </p>
           </div>
           <div ng-if="event.type == 'confirmed'">
             <p>{{ event.date }} {{ event.timeStart }} - {{event.timeEnd}} </p>
