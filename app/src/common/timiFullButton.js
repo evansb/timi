e30e634a78c06651e8c4843653841b08d480b90a
Evan Sebastian <evanlhoini@gmail.com>
2015-09-20 10:22:00 +0800

    Split small directives template to HTML

--- app/src/common/timiFullButton.js
+++ app/src/common/timiFullButton.js
@@ -7,13 +7,6 @@ export default () => {
     },
     restrict: 'E',
     transclude: true,
-    template:
-      `<div class="row timi-full-button">
-        <div class="col text-center">
-          <button class="button button-{{color}}" type="{{type}}">
-            <div ng-transclude></div>
-          </button>
-        </div>
-      </div>`
+    templateUrl: __dirname + '/timi-full-button.html'
   };
 }
--- app/src/common/timiPersonCard.js
+++ app/src/common/timiPersonCard.js
@@ -3,16 +3,7 @@ export default () => {
   return {
     restrict: 'E',
     transclude: true,
-    template:
-      `<div class="timi-person-card">
-        <div class="row">
-          <div class="item item-checkbox">
-            <label class="checkbox">
-              <input type="checkbox">
-            </label>
-            {{ participant.name }}
-          </div>
-        </div>
-      </div>`
+    templateUrl: __dirname + '/timi-person-card.html'
+
   };
 }
