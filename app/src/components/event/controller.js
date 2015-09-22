4dc47b4839070d77fe9113f38714d5cbaba33ee5
vsharonlynn <v.sharon.lynn@gmail.com>
2015-09-22 15:48:27 +0800

    Updated event invite behaviours

--- app/src/components/event/controller.js
+++ app/src/components/event/controller.js
@@ -9,6 +9,7 @@ export default ($scope, $location, $state, $stateParams, $timi) => {
         eventId: $stateParams.eventId
       }, (event) => {
         $scope.event = event;
+        console.log($scope.event);
       });
     } catch(err) {
       console.log(err);
@@ -25,5 +26,24 @@ export default ($scope, $location, $state, $stateParams, $timi) => {
 
   $scope.backToHome = () => {
     $state.go('home');
+  };
+
+  $scope.selected = 0;
+
+  $scope.clearSelection = () => {
+    for (var i = 0; i < $scope.event.timeslots.length; i++) {
+      $scope.event.timeslots[i].isSelected = false;
+    }
+    $scope.selected = 0;
+  };
+
+  $scope.selectTimeSlot = (id) => {
+    if ($scope.event.timeslots[id].isSelected) {
+      $scope.selected -= 1;
+      $scope.event.timeslots[id].isSelected = false;
+    } else {
+      $scope.selected += 1;
+      $scope.event.timeslots[id].isSelected = true;
+    }
   };
 }
