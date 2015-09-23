a9da1c5924308f10e4a003f32fec89ec1d1fc377
Evan Sebastian <evanlhoini@gmail.com>
2015-09-23 23:20:19 +0800

    Fix settings page

--- app/src/components/settings/controller.js
+++ app/src/components/settings/controller.js
@@ -1,11 +1,19 @@
 
-export default ($scope, $state, $auth, $timi) => {
+export default ($scope, $state, $auth, $timi, $rootScope) => {
+  $scope.user = {};
+
   $scope.backToHome = () => {
       $state.go('home')
   };
+
+  $rootScope.$on('meFetched', (e, me) => {
+    $scope.user = me;
+  });
 
   $scope.logout = () => {
     $auth.logout();
     $state.go('login');
   };
+
+  $timi.getActiveUser();
 }
