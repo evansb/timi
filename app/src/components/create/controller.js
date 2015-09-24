6428b4a9081f0a2b7238ef987fb4c47a80456b6e
vsharonlynn <v.sharon.lynn@gmail.com>
2015-09-24 15:57:21 +0800

    Add autocomplete location

--- app/src/components/create/controller.js
+++ app/src/components/create/controller.js
@@ -226,4 +226,42 @@ export default ($scope, $state, $timi, $rootScope, localStorageService) => {
   $scope.displayDuration = val => {
     return moment().startOf('day').add(val, 'milliseconds').format('HH [h] mm [min]');
   }
+
+
+  var placeSearch, autocomplete;
+
+  let initAutocomplete = () => {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+        {types: ['geocode']});
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', () => {
+      $scope.newEvent.latitude = autocomplete.getPlace();
+      console.log(autocomplete.getPlace());
+    });
+  }
+  window.initAutocomplete = initAutocomplete;
+  initAutocomplete();
+
+  // Bias the autocomplete object to the user's geographical location,
+  // as supplied by the browser's 'navigator.geolocation' object.
+  $scope.geolocate = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geolocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        var circle = new google.maps.Circle({
+          center: geolocation,
+          radius: position.coords.accuracy
+        });
+        autocomplete.setBounds(circle.getBounds());
+      });
+    }
+  };
 };
